07e8fd12baea46a1c0aa2f637ab1aaff3c280871
mandeep <minturajmdb1999@gmail.com>
2024-06-25 21:10:06 +0530

    update

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
-// app.use(express.static(path.join(__dirname, "./client/build")));
+ app.use(express.static(path.join(__dirname, "./client/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+ app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+ });
 
 app.get("/",(req,res)=>{
   res.send("working perfect....")
